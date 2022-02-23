394125409a022aeabea55b155c825d9ef2aa94a7
leewoonhyoung <wh3619@gmail.com>
2022-02-23 18:43:41 +0900

    separate ValidationForm

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,10 +1,8 @@
 package hello.itemservice.domain.item;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -14,17 +12,18 @@ import java.util.List;
 @Setter
 public class Item {
 
-    @NotNull
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(message = "공백은 입력할 수 없습니다.")
+ //   @NotBlank(message = "공백은 입력할 수 없습니다.", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min =1000, max = 1000000)
+  //  @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  //  @Range(min =1000, max = 1000000,groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price; // price 가 null 일 가능성이 존재한다.
 
-    @NotNull
+ //   @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+   // @Max(value =9999,  groups=SaveCheck.class)
     private Integer quantity;
 
     private Boolean open; // 판매여부
