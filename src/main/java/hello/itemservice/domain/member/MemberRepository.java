e51a2c44949cbcd65debacf66061c41ce0065b76
leewoonhyoung <wh3619@gmail.com>
2022-02-24 22:58:17 +0900

    add Login + Cookie

---
+++ src/main/java/hello/itemservice/domain/member/MemberRepository.java
@@ -0,0 +1,52 @@
+package hello.itemservice.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+    private static Long sequence = 0L;
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+//
+//    public Optional<Member> findByLongId(String loginId){
+//        List<Member> all = findAll();
+//        for(Member m : all){
+//            if (m.getLoginId().equals(loginId)){
+//                return Optional.of(m);
+//            }
+//        }
+//        return Optional.empty();
+//    }
+    public Optional<Member> findByLoginId(String loginId){
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+    public void clearStore(){
+        store.clear();
+    }
+
+}
